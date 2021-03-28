7dd14bf6b55ce91a4d9051c47bee1de7538a78ee
sumihiro3 <sumihiro@gmail.com>
2021-03-28 13:20:57 +0900

    implement as nuxt-composition-api

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -62,30 +62,49 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  data() {
+<script lang="ts">
+import { defineComponent, ref } from 'nuxt-composition-api'
+
+type MenuItem = {
+  icon: string
+  title: string
+  to: string
+}
+
+export default defineComponent({
+  setup() {
+    const clipped = ref(false)
+    const drawer = ref(false)
+    const fixed = ref(false)
+    const items = ref<MenuItem[]>([
+      {
+        icon: 'mdi-apps',
+        title: 'Welcome',
+        to: '/',
+      },
+      {
+        icon: 'mdi-chart-bubble',
+        title: 'Inspire',
+        to: '/inspire',
+      },
+    ])
+    const miniVariant = ref(false)
+    const right = ref(false)
+    const rightDrawer = ref(false)
+    const title = ref('Vuetify.js')
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      clipped,
+      drawer,
+      fixed,
+      items,
+      miniVariant,
+      right,
+      rightDrawer,
+      title,
     }
   },
-}
+  head: {
+    title: 'Sample',
+  },
+})
 </script>
