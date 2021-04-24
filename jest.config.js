f4c78b6a5422d954f17a13d2ad54a4db449f5b16
sumihiro3 <sumihiro@gmail.com>
2021-04-24 21:26:29 +0900

    fix test configs

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,27 @@
+process.env.NODE_ENV = 'test'
+
 module.exports = {
+  preset: 'ts-jest',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1',
     '^vue$': 'vue/dist/vue.common.js',
   },
   moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  testMatch: ['<rootDir>/test/**/*.js', '<rootDir>/test/**/*.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
   },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/src/components/**/*.vue',
+    '<rootDir>/src/pages/**/*.vue',
   ],
 }
