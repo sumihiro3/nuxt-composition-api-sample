d55b2f0bf9b9e02bed5907281d143d5c5149e7f3
sumihiro3 <sumihiro@gmail.com>
2021-04-24 18:31:56 +0900

    Separate the logics as functionality

--- src/pages/todos.vue
+++ src/pages/todos.vue
@@ -3,17 +3,17 @@
     <!-- Add ToDo -->
     <v-row>
       <v-col cols="4">
-        <v-text-field v-model="toDoTitle" type="text" />
+        <v-text-field v-model="todoTitleRef" type="text" />
       </v-col>
       <v-col cols="2">
-        <v-btn @click="addTodo">Add</v-btn>
+        <v-btn @click="addToDo">Add</v-btn>
       </v-col>
     </v-row>
     <!-- Search ToDo -->
     <v-row>
       <v-col cols="4">
         <v-text-field
-          v-model="searchText"
+          v-model="filterTitleTextRef"
           placeholder="Filter Todos"
           type="text"
         />
@@ -61,6 +61,7 @@
 </template>
 
 <script lang="ts">
+import { Ref } from '@vue/composition-api'
 import {
   defineComponent,
   ref,
@@ -76,11 +77,83 @@ interface ToDo {
   completed: boolean
 }
 
+const useToDoList = () => {
+  // ToDo List
+  const toDosRef = ref<ToDo[]>([])
+
+  // Change ToDo status
+  const changeToDoStatus = (toDo: ToDo, status: boolean) => {
+    const index = toDosRef.value.indexOf(toDo)
+    toDosRef.value.splice(index, 1, { ...toDo, completed: status })
+  }
+
+  return {
+    toDosRef,
+    changeToDoStatus,
+  }
+}
+
+const useAddingToDo = (toDosRef: Ref<ToDo[]>) => {
+  // new ToDo title
+  const todoTitleRef = ref<string>('')
+
+  // Add new ToDo
+  const addToDo = () => {
+    const id = Math.floor(new Date().getTime() / 1000)
+    const t: ToDo = {
+      id,
+      title: todoTitleRef.value,
+      completed: false,
+      userId: 10,
+    }
+    toDosRef.value.push(t)
+    todoTitleRef.value = ''
+  }
+  return {
+    todoTitleRef,
+    addToDo,
+  }
+}
+
+const useToDoFilter = (toDosRef: Ref<ToDo[]>) => {
+  // filter text
+  const filterTitleTextRef = ref<string>('')
+
+  // Filter todos by filter text
+  const filteredToDos = computed(() => {
+    return toDosRef.value.filter((t: ToDo) =>
+      t.title.includes(filterTitleTextRef.value)
+    )
+  })
+
+  // Filter doing todos
+  const doingToDos = computed(() => {
+    return filteredToDos.value.filter((t: ToDo) => t.completed === false)
+  })
+
+  // Filter completed todos
+  const completedToDos = computed(() => {
+    return filteredToDos.value.filter((t: ToDo) => t.completed === true)
+  })
+
+  return {
+    filterTitleTextRef,
+    filteredToDos,
+    doingToDos,
+    completedToDos,
+  }
+}
+
 export default defineComponent({
   setup() {
-    const toDoTitle = ref<string>('')
-    const searchText = ref<string>('')
-    const toDos = ref<ToDo[]>([])
+    const { toDosRef, changeToDoStatus } = useToDoList()
+    const { todoTitleRef, addToDo } = useAddingToDo(toDosRef)
+    const {
+      filterTitleTextRef,
+      filteredToDos,
+      doingToDos,
+      completedToDos,
+    } = useToDoFilter(toDosRef)
 
     // Load todos
     useAsync(async () => {
@@ -88,51 +161,21 @@ export default defineComponent({
       const todoList = await app.$axios.$get<ToDo[]>(
         'https://jsonplaceholder.typicode.com/todos'
       )
-      toDos.value = todoList.slice(0, 20)
+      toDosRef.value = todoList.slice(0, 20)
     })
-
-    // Filter todos by searchText
-    const searchedToDos = computed(() => {
-      return toDos.value.filter((t) => t.title.includes(searchText.value))
-    })
-
-    // Filter doing todos
-    const doingToDos = computed(() => {
-      return searchedToDos.value.filter((t) => t.completed === false)
-    })
-
-    // Filter completed todos
-    const completedToDos = computed(() => {
-      return searchedToDos.value.filter((t) => t.completed === true)
-    })
-
-    // Add new ToDo
-    function addTodo() {
-      const id = Math.floor(new Date().getTime() / 1000)
-      const t: ToDo = {
-        id,
-        title: toDoTitle.value,
-        completed: false,
-        userId: 10,
-      }
-      toDos.value.push(t)
-      toDoTitle.value = ''
-    }
-
-    // Change ToDo status
-    function changeToDoStatus(toDo: ToDo, status: boolean) {
-      toDo.completed = status
-    }
 
     return {
-      toDoTitle,
-      searchText,
-      toDos,
-      searchedToDos,
+      // Refs
+      toDosRef,
+      todoTitleRef,
+      filterTitleTextRef,
+      // Functions
+      changeToDoStatus,
+      addToDo,
+      // Computed
+      filteredToDos,
       doingToDos,
       completedToDos,
-      addTodo,
-      changeToDoStatus,
     }
   },
 })
